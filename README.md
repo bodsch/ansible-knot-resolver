07073ef54a11e9b36a90cebc747332eea746975a
Bodo Schulz <bodo@boone-schulz.de>
2021-08-19 20:30:53 +0200

    development

---
+++ README.md
@@ -0,0 +1,46 @@
+
+# Ansible Role:  `knot-resolver`
+
+This role will fully configure and install [knot-resolver](https://github.com/CZ-NIC/knot-resolver).
+
+[![GitHub Workflow Status](https://img.shields.io/github/workflow/status/bodsch/ansible-icinga2/CI)][ci]
+[![GitHub issues](https://img.shields.io/github/issues/bodsch/ansible-knot-resolver)][issues]
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/bodsch/ansible-knot-resolver)][releases]
+
+[ci]: https://github.com/bodsch/ansible-knot-resolver/actions
+[issues]: https://github.com/bodsch/ansible-knot-resolver/issues?q=is%3Aopen+is%3Aissue
+[releases]: https://github.com/bodsch/ansible-knot-resolver/releases
+
+
+## Requirements & Dependencies
+
+not known
+
+### Operating systems
+
+Tested on
+
+* Debian 9 / 10
+* Ubuntu 20.04
+* CentOS 8
+* OracleLinux 8
+* ArchLinux
+
+## configuration
+
+### default
+
+```yaml
+
+```
+
+
+## Author and License
+
+  - Bodo Schulz
+
+## License
+
+[Apache](LICENSE)
+
+`FREE SOFTWARE, HELL YEAH!`
---
+++ filter_plugins/resolver_listener.py
@@ -0,0 +1,62 @@
+# python 3 headers, required if submitting to Ansible
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+# from ansible.errors import AnsibleError, AnsibleParserError
+# from ansible.plugins.lookup import LookupBase
+from ansible.utils.display import Display
+
+# https://docs.ansible.com/ansible/latest/dev_guide/developing_plugins.html
+# https://blog.oddbit.com/post/2019-04-25-writing-ansible-filter-plugins/
+
+display = Display()
+
+
+class FilterModule(object):
+    """
+        Ansible file jinja2 tests
+    """
+
+    def filters(self):
+        return {
+            'resolver_listener': self.listener,
+        }
+
+
+    def listener(self, data):
+        result = ""
+        count = len(data)
+        # display.v("found: {} entries in {} {}".format(count, data, type(data)))
+
+        if isinstance(data, dict):
+            _interfaces = []
+            _ips = []
+            _port = 0
+            _options = []
+
+            interfaces = data.get('interfaces', [])
+            ips = data.get('ips', [])
+            port = data.get('port', '')
+            options = data.get('options', {})
+
+            if len(interfaces) > 0:
+                _interfaces = ("net." + ",net.".join(interfaces)).split(',')
+
+            if len(ips) > 0:
+                _ips = ( "'" + "','".join(ips) + "'").split(',')
+
+            if int(port) > 0:
+                _port = [ str(port) ]
+
+            if len(options) > 0:
+                for k, v in options.items():
+                    # -- {{ k }} - {{ v }}
+                    if k.lower() == 'tls' and v:
+                        _options = [ "{ tls = true }" ]
+
+            _listen = [ "{ " + ", ".join(_interfaces + _ips) + " }" ]
+            result = ", ".join(_listen + _port + _options)
+
+            # display.v("result {} {}".format(result, type(result)))
+
+            return result
