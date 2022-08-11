2e08ee79cecfd4c1f81dfbcf1f3228bb15ee7340
Bodo Schulz <bodo@boone-schulz.de>
2022-08-11 16:42:37 +0200

    fix test for artix

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -31,17 +31,29 @@ def get_vars(host):
 
     """
     base_dir, molecule_dir = base_directory()
+    distribution = host.system_info.distribution
+    operation_system = None
 
-    file_defaults = "file={}/defaults/main.yml name=role_defaults".format(base_dir)
-    file_vars = "file={}/vars/main.yml name=role_vars".format(base_dir)
-    file_molecule = "file={}/group_vars/all/vars.yml name=test_vars".format(molecule_dir)
+    if distribution in ['debian', 'ubuntu']:
+        operation_system = "debian"
+    elif distribution in ['redhat', 'ol', 'centos', 'rocky', 'almalinux']:
+        operation_system = "redhat"
+    elif distribution in ['arch', 'artix']:
+        operation_system = f"{distribution}linux"
+
+    file_defaults = f"file={base_dir}/defaults/main.yaml name=role_defaults"
+    file_vars = f"file={base_dir}/vars/main.yaml name=role_vars"
+    file_molecule = f"file={molecule_dir}/group_vars/all/vars.yml name=test_vars"
+    file_distibution = f"file={base_dir}/vars/{operation_system}.yaml name=role_distibution"
 
     defaults_vars = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
     vars_vars = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    distibution_vars = host.ansible("include_vars", file_distibution).get("ansible_facts").get("role_distibution")
     molecule_vars = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
 
     ansible_vars = defaults_vars
     ansible_vars.update(vars_vars)
+    ansible_vars.update(distibution_vars)
     ansible_vars.update(molecule_vars)
 
     templar = Templar(loader=DataLoader(), variables=ansible_vars)
@@ -53,12 +65,20 @@ def get_vars(host):
 def test_packages(host):
     """
     """
+    distribution = host.system_info.distribution
+    release = host.system_info.release
+
+    print(f"distribution: {distribution}")
+    print(f"release     : {release}")
+
     packages = []
     packages.append("knot-resolver")
 
-    for package in packages:
-        p = host.package(package)
-        assert p.is_installed
+    # artix ist not supported
+    if not distribution == "artix":
+        for package in packages:
+            p = host.package(package)
+            assert p.is_installed
 
 
 @pytest.mark.parametrize("dirs", [
